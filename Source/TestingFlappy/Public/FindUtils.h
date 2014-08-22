69082615e9028578eb8846379fe0331ccf587591
demonh3x <mat30041986@gmail.com>
2014-08-22 23:45:28 +0200

    Added default template type to FindUtils::ByName(FString)

--- Source/TestingFlappy/Public/FindUtils.h
+++ Source/TestingFlappy/Public/FindUtils.h
@@ -11,12 +11,12 @@ public:
 	FindUtils(UWorld* World) : World(World){};
 	~FindUtils(){};
 
-	template <typename T> T* ByName(FString Name) {
-		UE_LOG(LogTemp, Warning, TEXT("finding actor with name %s"), *Name);
+	template <typename T = AActor> T* ByName(FString Name) {
+		//UE_LOG(LogTemp, Warning, TEXT("finding by name %s"), *Name);
 		for (TActorIterator<T> ActorItr(World); ActorItr; ++ActorItr) {
-			UE_LOG(LogTemp, Warning, TEXT("%s: %s"), *ActorItr->GetName(), *ActorItr->GetActorLocation().ToString());
+			//UE_LOG(LogTemp, Warning, TEXT("%s: %s"), *ActorItr->GetName(), *ActorItr->GetActorLocation().ToString());
 			if (ActorItr->GetName().Equals(Name, ESearchCase::CaseSensitive)){
-				UE_LOG(LogTemp, Warning, TEXT("Found %s"), *Name);
+				//UE_LOG(LogTemp, Warning, TEXT("Found %s"), *Name);
 				return *ActorItr;
 			}
 		}
